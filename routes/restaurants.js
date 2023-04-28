7b5dc3d7aa4663b62e5947c63269f1c512dd2eaf
Joe Caruso <carusoj7@gmail.com>
2023-04-27 20:54:35 -0500

    edit readme

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,7 +6,6 @@ import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-// GET localhost:3000/restaurants
 router.get('/', isLoggedIn, restaurantsCtrl.index)
 
 router.get('/new', isLoggedIn, restaurantsCtrl.new)
@@ -17,25 +16,16 @@ router.get('/:restaurantId/edit', isLoggedIn, restaurantsCtrl.edit)
 
 router.get('/:restaurantId/reviews/:reviewId/edit', isLoggedIn, restaurantsCtrl.editReview)
 
-//POST localhost:3000/restaurants
-
 router.post('/', isLoggedIn, restaurantsCtrl.create)
 
 router.post('/:restaurantId/reviews', isLoggedIn, restaurantsCtrl.addReview)
-
-// PUT localhost:3000/restaurants/:restaurantId
 
 router.put('/:restaurantId', isLoggedIn, restaurantsCtrl.update)
 
 router.put('/:restaurantId/reviews/:reviewId', isLoggedIn, restaurantsCtrl.updateReview)
 
-// DELETE localhost:3000/restaurants/:restaurantId
 router.delete('/:restaurantId', isLoggedIn, restaurantsCtrl.delete)
 
-// DELETE localhost:3000/restaurants/:restaurantId/reviews/:reviewId
-
 router.delete('/:restaurantId/reviews/:reviewId', isLoggedIn, restaurantsCtrl.deleteReview)
-
-
 
 export{ router }
